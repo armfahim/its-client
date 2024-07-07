68f5430e05cd86ad6f52fff0c5ffc1977de7aa6c
fahim <fahim@eatlbd.com>
2024-07-07 18:48:46 +0600

    receive obj from behavior subject in paperwork add component

--- src/app/all-modules/paperworks/paperworks-add/paperworks-add.component.ts
+++ src/app/all-modules/paperworks/paperworks-add/paperworks-add.component.ts
@@ -1,27 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CalendarEvent } from 'angular-calendar';
+import { Subscription } from 'rxjs';
+import { PaperworksService } from '../../services/paperworks.service';
 
 @Component({
   selector: 'app-paperworks-add',
   templateUrl: './paperworks-add.component.html',
   styleUrls: ['./paperworks-add.component.css']
 })
-export class PaperworksAddComponent implements OnInit {
+export class PaperworksAddComponent implements OnInit, OnDestroy  {
 
   viewDate: Date = new Date();
   events: CalendarEvent[] = [];
 
   public addPaperworkForm: FormGroup;
+  subscription: Subscription;
+  sharedPaperworkObj: any;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder,
+    private paperworksService: PaperworksService,
+  ) { }
 
   ngOnInit(): void {
+    this.subscription = this.paperworksService.currentPaperwork.subscribe(obj => this.sharedPaperworkObj = obj);
+
+
+
     this.addPaperworkForm = this.formBuilder.group({
       paperworkDate: ["", [Validators.required]],
       items: this.formBuilder.array([]),
-      // itemName: ["", []],
-      // cashPurchaseAmount: ["", []],
     })
     this.addItem();
   }
@@ -41,6 +49,10 @@ export class PaperworksAddComponent implements OnInit {
         cashPurchaseAmount: [''],
       })
     )
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
 }
